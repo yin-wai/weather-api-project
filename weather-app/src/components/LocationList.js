73bb9687f1f87ca77d846a87288485850f0ca8a0
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-27 15:25:36 +0100

    handling submits

--- weather-app/src/components/LocationList.js
+++ weather-app/src/components/LocationList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import LocationCard from './LocationCard'
+import Api from './helper/Api'
 
 const LocationList = () => {
     const [locations, setLocations] = useState([
@@ -15,7 +16,7 @@ const LocationList = () => {
             "localtime": "2021-10-27 14:46"
         }
     ])
-    console.log(locations)
+
     useEffect(() => {
      async function fetchLocation() {
 
@@ -34,16 +35,41 @@ console.log(locations)
 }
 fetchLocation()
 }, [])
+
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        console.log(event.target.value)
+        const locationSubmit = event.target.value
+        const config = {
+            method: 'get',
+            url: `http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=${locationSubmit}`,
+            headers: { 
+                        'api': '8b928a9753d74262887160151212610'
+             }
+};
+
+const response = await axios(config)
+setLocations(response.data)
+console.log(response.data)
+console.log(locations)
+}
+
     return (
+        <>
+        <form>
+            <label>select city</label>
+            <input type="search" value="city">
+            </input><input type='submit' onSubmit={handleSubmit}></input>
+        </form>
         <div>
             <ul>
-            
                 <li>
                     <p>{locations.location.name}</p>
                 </li>
-            
             </ul>
         </div>
+        </>
     )
 }
 
---
+++ weather-app/src/components/helper/Api.js
@@ -0,0 +1,3 @@
+const baseUrl = 'http://api.weatherapi.com/v1/'
+const apikey = '8b928a9753d74262887160151212610'
+const current = '/current.json'
