f483fadb571dcfde09822bfa59e7f5a42d82836f
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-27 17:15:02 +0100

    adding formvalue and formsubmit

--- weather-app/src/components/LocationList.js
+++ weather-app/src/components/LocationList.js
@@ -4,78 +4,49 @@ import LocationCard from './LocationCard'
 import Api from './helper/Api'
 
 const LocationList = () => {
-    const [locations, setLocations] = useState([
-        {
-            "name": "Paris",
-            "region": "Ile-de-France",
-            "country": "France",
-            "lat": 48.87,
-            "lon": 2.33,
-            "tz_id": "Europe/Paris",
-            "localtime_epoch": 1635338775,
-            "localtime": "2021-10-27 14:46"
-        }
-    ])
+    const [locations, setLocations] = useState([])
+    const [formValue, setFormValue] = useState('')
+    const [formSubmit, setFormSubmit] = useState('')
 
-//     useEffect(() => {
-//      async function fetchLocation() {
-
-//         const config = {
-//             method: 'get',
-//             url: 'http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=paris',
-//             headers: { 
-//                         'api': '8b928a9753d74262887160151212610'
-//              }
-// };
-
-// const response = await axios(config)
-// setLocations(response.data)
-// console.log(response.data)
-// console.log(locations)
-// }
-// fetchLocation()
-// }, [])
-
-
-    async function handleSubmit(event) {
-        event.preventDefault()
-        console.log(event.target.value)
-        const locationSubmit = event.target.value 
+    useEffect(() => {
+     async function fetchLocation() {
         const config = {
             method: 'get',
-            url: `http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=${locationSubmit}`,
+            url: `http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=${formValue}`,
             headers: { 
                         'api': '8b928a9753d74262887160151212610'
              }
 };
 try {
-    
-
 const response = await axios(config)
 setLocations(response.data)
 console.log(response.data)
 console.log(locations)
-} catch(err) {}
+} catch (err) {}
 }
+fetchLocation()
+}, [formSubmit])
 
 const handleFormChange = (event) => {
-    const{name,value} = event.target
-    setLocations({...locations,[name]:value})
+    console.log(event.target.value)
+    setFormValue(event.target.value)
+ }
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    setFormSubmit(formValue)
 }
 
-const formInputProp = { data: locations, handleFormChange}
+
     return (
         <>
         <form onSubmit={handleSubmit}>
-            <label>input city</label>
-            <input type="text">
-            </input><input type='submit'></input>
-            <LocationCard />
+            <input onChange={handleFormChange}></input>
         </form>
         <div>
             <ul>
                 <li>
-                    {/* <p>{locations.location.name}</p> */}
+                    <p>{locations.location.name}</p>
                 </li>
             </ul>
         </div>
