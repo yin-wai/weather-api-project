d272a9dbbf5bbe0f41415cd32276301c536c77e5
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-27 16:08:29 +0100

    unresolved event.target.value

--- weather-app/src/components/LocationList.js
+++ weather-app/src/components/LocationList.js
@@ -17,30 +17,30 @@ const LocationList = () => {
         }
     ])
 
-    useEffect(() => {
-     async function fetchLocation() {
+//     useEffect(() => {
+//      async function fetchLocation() {
 
-        const config = {
-            method: 'get',
-            url: 'http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=paris',
-            headers: { 
-                        'api': '8b928a9753d74262887160151212610'
-             }
-};
+//         const config = {
+//             method: 'get',
+//             url: 'http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=paris',
+//             headers: { 
+//                         'api': '8b928a9753d74262887160151212610'
+//              }
+// };
 
-const response = await axios(config)
-setLocations(response.data)
-console.log(response.data)
-console.log(locations)
-}
-fetchLocation()
-}, [])
+// const response = await axios(config)
+// setLocations(response.data)
+// console.log(response.data)
+// console.log(locations)
+// }
+// fetchLocation()
+// }, [])
 
 
     async function handleSubmit(event) {
         event.preventDefault()
         console.log(event.target.value)
-        const locationSubmit = event.target.value
+        const locationSubmit = event.target.value 
         const config = {
             method: 'get',
             url: `http://api.weatherapi.com/v1/current.json?key=8b928a9753d74262887160151212610&q=${locationSubmit}`,
@@ -48,24 +48,34 @@ fetchLocation()
                         'api': '8b928a9753d74262887160151212610'
              }
 };
+try {
+    
 
 const response = await axios(config)
 setLocations(response.data)
 console.log(response.data)
 console.log(locations)
+} catch(err) {}
 }
 
+const handleFormChange = (event) => {
+    const{name,value} = event.target
+    setLocations({...locations,[name]:value})
+}
+
+const formInputProp = { data: locations, handleFormChange}
     return (
         <>
-        <form>
-            <label>select city</label>
-            <input type="search" value="city">
-            </input><input type='submit' onSubmit={handleSubmit}></input>
+        <form onSubmit={handleSubmit}>
+            <label>input city</label>
+            <input type="text">
+            </input><input type='submit'></input>
+            <LocationCard />
         </form>
         <div>
             <ul>
                 <li>
-                    <p>{locations.location.name}</p>
+                    {/* <p>{locations.location.name}</p> */}
                 </li>
             </ul>
         </div>
