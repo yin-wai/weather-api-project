916cc4fa06a8df0643d4b33af75cdcb7d288691b
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-28 13:42:24 +0100

    background images

--- weather-app/src/components/helper/getWeather.js
+++ weather-app/src/components/helper/getWeather.js
@@ -12,7 +12,7 @@ export const willItRain = (rain) => {
     if (rain === 0 ) {
         return 'no-rain'
     } else if (rain === 1 ) {
-        return 'snow'
+        return 'rain'
     }
 }
 
@@ -21,5 +21,17 @@ export const willItSnow = (snow) => {
         return 'no-snow'
     } else if (snow === 1) {
         return 'snow'
+    }
+}
+
+export const getWind = (wind) => {
+    if (wind >= 50) {
+        return "extremely windy"
+    } else if (wind >= 30) {
+        return "windy"
+    } else if (wind >=15) {
+        return "breezy"
+    } else {
+        return "no wind"
     }
 }
