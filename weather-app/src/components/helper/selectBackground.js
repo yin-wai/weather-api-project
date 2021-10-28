1e8ffb19b2e8238c59d77dcb218fb8f895dc2bb8
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-28 17:37:39 +0100

    background edits

--- weather-app/src/components/helper/selectBackground.js
+++ weather-app/src/components/helper/selectBackground.js
@@ -1,9 +1,18 @@
 import cloud from '../../styles/images/cloud.jpg'
+import sunny from '../../styles/images/sunny.jpg'
+import rain from '../../styles/images/rain.jpg'
+import snow from '../../styles/images/snow.jpg'
 
 export const selectBackground = (cloudsProp, rainProp, snowProp) => {
-    if (cloudsProp === 'cloudy') {
-        return cloud 
-    } else if () {
-
+    if (cloudsProp === 'cloudy' && rainProp === false ) {
+        return 
+    } else if (cloudsProp === 'some-clouds' && rainProp === false) {
+        return cloud
+    } else if (cloudsProp === 'clear' && rainProp === false) {
+        return sunny
+    } else if ((cloudsProp === "cloudy" || cloudsProp === "some-clouds") && rainProp === true) {
+        return rain
+    } else if (snowProp === true) {
+        return snow
     }
 }
