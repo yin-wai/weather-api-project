74532b1851a678870788ae7e2cdd1a467f39f6be
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2021-10-28 13:39:13 +0100

    added pages with links

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,13 +1,25 @@
-import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home';
+import Nav from './components/Nav'
+import Astronomy from './pages/Astronomy';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <Home />
+        <Nav />
       </header>
+      <main>
+        <Switch>
+          <Route path='/astronomy.json' component={Astronomy} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </main>
     </div>
+    </Router>
   );
 }
 
--- weather-app/src/pages/Astronomy.js
+++ weather-app/src/pages/Astronomy.js
@@ -5,24 +5,13 @@ import axios from 'axios'
 
 const Astronomy = () => {
     const [locations, setLocations] = useState('')
-    const [currentCondition, setCurrentCondition] = useState('')
-    const [currentTemp, setCurrentTemp] = useState('')
-    const [forecast, setForecast] = useState('')
-    
     const [locationFormValue, setLocationFormValue] = useState('')
-    
     const [formSubmit, setFormSubmit] = useState('')
     
-    
-    
-    
-    
-
     const handleLocationChange = (event) => {
         setLocationFormValue(event.target.value)     
       }
 
-    
     const handleSubmit = (event) => {
         event.preventDefault()
         setFormSubmit(locationFormValue)
@@ -30,18 +19,15 @@ const Astronomy = () => {
 
     const handleFormReset = () => {
         setLocations('')
-        setCurrentCondition('')
         setFormSubmit('')
         setLocationFormValue('')
-        setCurrentTemp('')
-        setForecast('')
     }
 
     useEffect(() => {
      async function fetchLocation() {
       const config = {
         method: 'get',
-        url: `http://api.weatherapi.com/v1/forecast.json?key=8b928a9753d74262887160151212610&q=${formSubmit}&days=1&aqi=yes&alerts=yes&lang=eng&hour=20`,
+        url: `http://api.weatherapi.com/v1/astronomy.json?key=8b928a9753d74262887160151212610&q=${formSubmit}`,
         headers: { 
                 'api': '8b928a9753d74262887160151212610'
              }
@@ -49,13 +35,7 @@ const Astronomy = () => {
 try {
 const response = await axios(config)
 console.log('response.data.location', response.data.location)
-console.log('response.data.current.condition.text', response.data.current.condition.text)
-console.log('response.data.forecast', response.data.forecast.forecastday)
-const forecastDay = response.data.forecast.forecastday
-setForecast(forecastDay[0].day.condition.text)
-setLocations(response.data.location)
-setCurrentCondition(response.data.current.condition.text)
-setCurrentTemp(response.data.current.temp_c)
+setLocations(response.data)
 } catch (err) {}
 }
 fetchLocation()
@@ -67,16 +47,13 @@ fetchLocation()
         <div>
         <form onSubmit={handleSubmit}>
             <input onChange={handleLocationChange}></input>
-            {/* <input onChange={handleTimeChange}></input>
-            <input onChange={handleLangChange}></input> */}
         </form>
         </div>
         :
         <div>
              <p>
-               In {locations.name}, at the moment, the weather is {currentCondition.toLocaleLowerCase()} with a temperature of<span> </span>
-               {currentTemp} degree celsius. The forecast for the rest of the day is {forecast.toLocaleLowerCase()}. 
-               {/* At {timeFormValue} the forecast is {timeCondition}.  */}
+               In {locations.location.name}, the sun will rise at {locations.astronomy.astro.sunrise} and set at {locations.astronomy.astro.sunset}. The moon will rise at {locations.astronomy.astro.moonrise} and set at {locations.astronomy.astro.moonset}. 
+               The moon cylce is in its {locations.astronomy.astro.moon_phase}.
              </p>
              <button onClick={handleFormReset}>search different location</button>
         </div>
