398ebae99db91c7590f6b3da817742aa9c4ec881
shahrukh-hughes <s.khan_1987@hotmail.co.uk>
2022-02-17 14:10:48 +0000

    edited forecast page styling

---
+++ weather-app/src/components/ForecastDay2.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { correctFormat } from "../helper/SortForecast";
+import ForecastDayExtra2 from "./ForecastDayExtra2";
+
+const ForecastDay2 = ({ day, date, hour }) => {
+  const [displayExtra, setDisplayExtra] = useState(false);
+  const toggleDisplay = () => {
+    setDisplayExtra(!displayExtra);
+  };
+  const displayShowHide = displayExtra ? "Expand" : "Condense";
+
+  return (
+    <>
+      <div className="weather-summary-container">
+        <div className="weather-summary">
+          <p>Date: {correctFormat(date)}</p>
+          <p>Average Temperature:</p>
+          <p>Average Humidity:</p>
+          <p>Chance of rain:</p>
+          <p>Chance of snow:</p>
+        </div>
+        <div className="weather-summary">
+          <p>{day.condition.text.toLowerCase()}</p>
+          <p>{day.avgtemp_c} &#8451;</p>
+          <p>{day.avghumidity} g/kg</p>
+          <p>{day.daily_chance_of_rain} &#37;</p>
+          <p>{day.daily_chance_of_snow} &#37;</p>
+        </div>
+        <div id="expand-div">
+          <span onClick={toggleDisplay}>{displayShowHide}</span>
+        </div>
+        {!displayExtra ? (
+          <>
+            <ForecastDayExtra2 hour={hour} />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ForecastDay2;
